131dfc25c06fd69a3c649c3abf133707597ef9b7
Raghu <Raghu@Raghu-PC>
2017-11-04 13:50:04 +0530

    first commit uploading all files

---
+++ SSPAPI/src/test/java/com/st/H.java
@@ -0,0 +1,25 @@
+package com.st;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class H {
+
+    public static void main(String[] args) {
+    	System.setProperty("webdriver.firefox.marionette","C:\\geckodriver.exe");
+    	WebDriver driver = new FirefoxDriver();
+        String baseUrl = "http://automationpractice.com/index.php";
+        String tagName = "";
+        
+        driver.get(baseUrl);
+        //tagName = driver.findElement(By.id("email")).getTagName();
+        //System.out.println(tagName);
+        
+        
+        
+        driver.close();
+        System.exit(0);
+}
+	
+	
+}
